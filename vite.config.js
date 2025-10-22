5f9d69c98966b1944ff7889aff44816b80053f98
Adonisnu4 <adonisnunez5@gmail.com>
2025-10-21 22:15:36 -0300

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'kine_app_admin'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`, 
   build: {
     outDir: 'build' 
   }
